7d5d20c7a7d4067f0dd255b1a5c34a5cfbe4c698
lunadancer@gmail.com <lichking123>
2016-02-24 20:11:45 +0800

    add load report for service locator

--- src/main/java/io/github/junheng/akka/locator/ServiceLocation.java
+++ src/main/java/io/github/junheng/akka/locator/ServiceLocation.java
@@ -6,6 +6,13 @@ public class ServiceLocation {
     public Double load;
     public String status;
 
+    public ServiceLocation() {
+        this.type = null;
+        this.url = null;
+        this.load = -0.0;
+        this.status = null;
+    }
+
     public ServiceLocation(String type, String url, Double load, String status) {
         this.type = type;
         this.url = url;
